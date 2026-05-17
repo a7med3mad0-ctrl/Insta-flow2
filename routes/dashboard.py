980909c6ba51961e9f682c4e6f2ea3cfac55322f
Mhmd-png <mohamed3mad.b6@gmail.com>
2026-05-17 12:45:02 +0300

    Create dashboard.py

---
+++ routes/dashboard.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("dashboard.html", {"request": request})
+
+
+@router.get("/dashboard", response_class=HTMLResponse)
+async def dashboard(request: Request):
+    return templates.TemplateResponse("dashboard.html", {"request": request})
