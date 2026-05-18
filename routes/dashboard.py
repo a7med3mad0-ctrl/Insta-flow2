a05ec5e92fc361721a90e351efd6232d20897a13
a7med3mad0-ctrl <a7med.3mad0@gmail.com>
2026-05-18 04:20:30 +0300

    Update dashboard.py

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,17 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from fastapi import Request
+import os
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-
 @router.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("dashboard.html", {"request": request})
-
+    return templates.TemplateResponse(
+        request=request,
+        name="dashboard.html"
+    )
 
 @router.get("/dashboard", response_class=HTMLResponse)
 async def dashboard(request: Request):
-    return templates.TemplateResponse("dashboard.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="dashboard.html"
+    )
